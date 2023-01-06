7b306f973c19d3840e6cc19171656787b70afdc9
mihika14 <mihikasaxena13@gmail.com>
2023-01-07 03:00:18 +0530

    content

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,35 +1,36 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-import { Input , Button } from 'semantic-ui-react';
-import './Homepage.css'
- 
+import { Input, Button } from 'semantic-ui-react';
+import './Homepage.css';
+import image1 from './ILLUSTRATION.png';
+
 const Homepage = () => {
 
   return (
     <div>
       < Navbar />
       <div className="rightcontent">
-        <Input icon='search' placeholder='Search |Topic' />     
+        <Input icon='search' placeholder='Search |Topic' />
       </div>
-      <div className="image">
-      <img src='.../Assets/ILLUSTRATION.png' alt='Loading' />
-      </div>
-      <div className="leftcontent">
+        <img className="illustration" src={image1} alt='Loading' />
+      {/* <div className="leftcontent"> */}
+      <>
         <h2 className="claim">Claim a Free Quote</h2>
-        <h2 className="content">Get started on fulfilling
-          your Dubai or UAE
+        <h2 className="maincontent">Get started on fulfilling<br/>
+          your Dubai or UAE <br/>
           dream.</h2>
         <h6 className="subcontent">UAE & Offshore Company</h6>
-        <p className="extracontent">We provide you with information about UAE or
-          Offshore Company Registration & help you
-          setup your company with a bank account and
+        <p className="extracontent">We provide you with information about UAE <br/>or
+          Offshore Company Registration & help you<br/>
+          setup your company with a bank account and<br/>
           visas</p>
-        <Button.Group >
-          <Button >Start a Company</Button>
+        <Button.Group className="company">
+          <Button>Start a Company</Button>
           <Button>Renew A company</Button>
         </Button.Group>
-      </div>
+        </>
       <div className="middle">Watch the video about UAE or Offshore Company Registration</div>
+      
     </div>
   )
 }
